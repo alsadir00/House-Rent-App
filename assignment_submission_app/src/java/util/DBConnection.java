3e7ed8de45ba2a06e5027fdf7ba6aca734308f67
alsadir00 <alsadir00@gmail.com>
2026-01-04 14:19:20 +0300

    db connection utility

---
+++ assignment_submission_app/src/java/util/DBConnection.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/projectportal";
+    private static final String USER = "root";      
+    private static final String PASSWORD = "De122924";      
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
